fdee4b9666378516f16c53114a847c9fe16f787e
Frederik Jacques <frederik@the-nerd.be>
2017-11-21 11:10:10 +0100

    Initial Commit

---
+++ RxDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxDemo
+//
+//  Created by Frederik Jacques on 21/11/2017.
+//  Copyright © 2017 the-nerd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
